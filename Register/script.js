70f696e31b3bd554f7b7fe3b39877fd7c7aa8fdf
manwesulimo1 <harshitraj.lohani123@gmail.com>
2020-07-30 22:59:26 +0530

    Completed javascript

--- Register/script.js
+++ Register/script.js
@@ -38,17 +38,109 @@ function emailValidation() {
     }   
 }
 
+function phoneValidation() {
+    var phone = document.getElementById("txt-phone").value;
+    
+    var regex = /^[0-9]{10}$/;
+    
+    if(regex.test(phone))
+    {
+        document.getElementById("txt-phone").className = "input-valid";
+        return true;
+    }      
+    else
+    {
+        document.getElementById("txt-phone").className = "input-invalid";
+        return true;
+    }
+}
+
+
+function countryValidation() {
+    var countryLength = document.getElementById("txt-country").value.length;
+    
+    if(countryLength == 0)
+    {
+        document.getElementById("txt-country").className = "input-invalid";
+        return false;
+    }
+    else
+    {
+        document.getElementById("txt-country").className = "input-valid";
+        return true;
+    }
+        
+}
+
+function positionValidation() {
+    var position = document.getElementById("select-position").value;
+    
+    if(position == "select")
+    {
+        document.getElementById("select-position").className = "input-invalid";
+        return false;
+    }
+    else
+    {
+        document.getElementById("select-position").className = "input-valid";
+        return true;
+    }
+        
+}
+
+
+function passwordValidation() {
+    var password = document.getElementById("txt-password1").value;
+    var passwordLength = password.length;
+    
+    if(passwordLength < 8)
+    {
+        document.getElementById("txt-password1").className = "input-invalid";
+        return false;
+    }
+    else
+    {
+        document.getElementById("txt-password1").className = "input-valid";
+        return false;
+    }
+}
+
+function rePasswordValidation() {
+    var password = document.getElementById("txt-password1").value;
+    var rePassword = document.getElementById("txt-password2").value;
+    
+    if(password != rePassword)
+    {
+        document.getElementById("txt-password2").className = "input-invalid";
+        return false;
+    }
+    else
+    {
+        document.getElementById("txt-password2").className = "input-valid";
+        return true;
+    }
+}
 
 
 function validateForm(){
-    
     var emailValid = emailValidation();
     var nameValid = nameValidation();
+    var passwordValid = passwordValidation();
+    var rePasswordValid = rePasswordValidation();
+    var countryValid = countryValidation();
+    var phoneValid = phoneValidation();
+    var positionValid = positionValidation();
     
-    if(emailValid || nameValid)
-        alert("Welcome");
+    
+    if(emailValid || nameValid || passwordValid)
+    {
+        alert("Welcome!");
+        document.getElementById("anchorID").click();
+    }
     else
-        alert("ERROR");
+    {
+        alert("Check the red marked fields!");
+    }
 }
 
 
